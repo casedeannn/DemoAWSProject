d457041cefeda6198e48222ab9337f5ab3c18cd4
Casey <casey.deannn@gmail.com>
2022-11-15 16:44:52 -0600

    First Commit

---
+++ src/main/java/com/casey/demoawsproject/DemoAwsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.casey.demoawsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAwsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAwsProjectApplication.class, args);
+	}
+
+}
