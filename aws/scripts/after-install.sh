19bd53c42a1c388ffab31e506694278742d9926a
Casey <casey.deannn@gmail.com>
2022-11-16 17:57:27 -0600

    Update

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -6,5 +6,5 @@ set -xe
 aws s3 cp s3://teststack-webappdeploymentbucket-lze2yjuqngj5/DemoAwsProject.jar /usr/local/tomcat9/webapps/DemoAwsProject.jar
 
 
-# Ensure the ownership permissions are correct.
+# Ensure the ownership permissions are correct
 chown -R tomcat:tomcat /usr/local/tomcat9/webapps
