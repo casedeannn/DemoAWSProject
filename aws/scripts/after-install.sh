bacf1c617d1ff4aa125d04727f2a1f81e1b46a07
Casey <casey.deannn@gmail.com>
2022-11-16 16:07:06 -0600

    fixing after-install

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://##s3-bucket##/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://teststack-webappdeploymentbucket-lze2yjuqngj5/DemoAwsProject.jar /usr/local/tomcat9/webapps/DemoAwsProject.jar
 
 
 # Ensure the ownership permissions are correct.
