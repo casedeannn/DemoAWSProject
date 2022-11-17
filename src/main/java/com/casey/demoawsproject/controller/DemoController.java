786c9ef40fd9048fcd718767b8d2683afdb08e5c
Casey <casey.deannn@gmail.com>
2022-11-16 18:44:47 -0600

    Test Controller

---
+++ src/main/java/com/casey/demoawsproject/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.casey.demoawsproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/casey")
+    public String hello() {
+        return "Hello world";
+    }
+
+
+    @GetMapping("/test")
+    public String hello2() {
+        return "world Hello";
+    }
+}
