3d12617b993be2466e6901e3d583f93d537237bc
Casey <casey.deannn@gmail.com>
2022-11-16 18:06:23 -0600

    Change naming

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://teststack-webappdeploymentbucket-lze2yjuqngj5/DemoAwsProject.jar /usr/local/tomcat9/webapps/DemoAwsProject.jar
+aws s3 cp s3://teststack-webappdeploymentbucket-lze2yjuqngj5/DemoAWSProject-0.0.1-SNAPSHOT.jar /usr/local/tomcat9/webapps/DemoAWSProject-0.0.1-SNAPSHOT.jar
 
 
 # Ensure the ownership permissions are correct
